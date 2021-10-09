3535e761d1cd89faa480c3e6d1e9071b4634d1c3
maicol <mmoarias4@gmail.com>
2021-10-09 14:17:29 -0500

    correccion

--- init.sh
+++
@@ -1,4 +0,0 @@
-git init
-git branch -M main
-git remote add origin https://github.com/MaicolM11/taller-electiva.git
-git push -u origin main
--- init_db.sql
+++ init_db.sql
@@ -9,13 +9,5 @@ start co_empleados.sql
 
 show errors;
 
-/*start co_productos.sql
-start co_ventas.sql
-start co_facturas.sql
-start co_empleados.sql*/
-
--- creacion de colecciones
-
-
--- migracion bd
+-- migracion bd MULTISET
 
